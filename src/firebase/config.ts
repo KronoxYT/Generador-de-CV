6a33b5719396782b5151ac3b323bdb67357af399
Keyler Tamayo <keylertamayo@gmail.com>
2025-12-26 03:27:09 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2075916503-6080f",
+  "appId": "1:680053190442:web:2a68c5760aca1c5e60b853",
+  "apiKey": "AIzaSyB57JsaWsLOr9-mhN829EWRPKEY7GzXyME",
+  "authDomain": "studio-2075916503-6080f.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "680053190442"
+};
